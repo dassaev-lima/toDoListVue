1ba7a9f03fceff9fd58ddad909652dd41ac130c1
dassaev-lima <dassaevnascimento@gmail.com>
2022-06-21 09:46:45 -0300

    adicionando icone ao botão de adicionar do form, e formantando o valor dos cards para duas casas decimais

--- src/components/CardBill.vue
+++ src/components/CardBill.vue
@@ -6,10 +6,10 @@
                     <h5 style="font-wigth:300" class="pt-1">{{ name }}</h5>
                 </div>
                 <div v-if="typeBill == 'Revenue'" class="ccol-sm-3 col-md-3 col-lg-3">
-                    <h5  class="text-green text-semibold pt-1">$ {{ value }}</h5>
+                    <h5  class="text-green text-semibold pt-1">$ {{ value.toFixed(2) }}</h5>
                 </div>
                 <div v-else class="ccol-sm-3 col-md-3 col-lg-3">
-                    <h5 class="text-red text-semibold pr-3 pt-1">$ {{ value }}</h5>
+                    <h5 class="text-red text-semibold pr-3 pt-1">$ {{ value.toFixed(2) }}</h5>
                 </div>
 
                 <div v-if="typeBill == 'Revenue'" class="col-sm-2 col-md-2 col-lg-1">
--- src/components/FormNewBill.vue
+++ src/components/FormNewBill.vue
@@ -2,7 +2,7 @@
     <div class="container text-left">
         <form class="form-group">
             <div class="row">
-                <div class="col-sm-12 col-md-6 col-lg-4 mb-3">
+                <div class="col-sm-12 col-md-6 col-lg-3 mb-3">
                     <label for="nameOfBill">Name Of Bill</label>
                     <input id="nameOfBill" type="text" v-model="form.name" class="form-control w-100" required="required">
                 </div>
@@ -10,7 +10,7 @@
                     <label for="valueOfBill">Value Of Bill</label>
                     <input id="valueOfBill" type="number" v-model="form.value" class="form-control w-100" required="required">
                 </div>
-                <div class="col-sm-6 col-md-6 col-lg-2 mt-4 mb-3">
+                <div class="col-sm-6 col-md-6 col-lg-3 mt-4 mb-3">
                     <div class="form-check">
                         <input class="form-check-input" type="radio" id="radioExpense" value="Expense" checked v-model="form.typeBill">
                         <label class="form-check-label" for="radioExpense">
@@ -25,7 +25,9 @@
                     </div>
                 </div>
                 <div class="col-sm-6 col-md-6 col-lg-2 mt-4 pt-1">
-                    <button type="submit" class="btn btn-green btn-lg form-control text-center" @click="this.$parent.addBill">Add</button>
+                    <button type="submit" class="btn btn-green btn-lg form-control text-center" @click="this.$parent.addBill">
+                        Add <span class="fas fa-plus ml-2"></span>
+                    </button>
                 </div>
             </div>
         </form>
